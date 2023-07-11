e82c7d939f55dcfa05fced1ac58350701a85fd1e
Nils Otto <nilsotto@gmx.de>
2023-07-11 13:05:34 +0200

    replace MainColor-Component and add state

--- components/MainColor/index.js
+++ components/MainColor/index.js
@@ -1,17 +1,31 @@
-import styled from "styled-components";
+import { Fragment } from "react";
 
-export default function MainColor({ text, id }) {
+export default function MainColor({ color, handleColorChange }) {
+  const colors = [
+    { id: "color1", text: "color1", name: "color1" },
+    { id: "color2", text: "color2", name: "color2" },
+    { id: "color3", text: "color3", name: "color3" },
+  ];
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    handleColorChange(id, value);
+  };
+
   return (
-    <>
-      <StyledLabel>
-        <label htmlFor={id}>{text}</label>
-      </StyledLabel>
-      <input type="color" id={id} />
-    </>
+    <Fragment>
+      {colors.map(({ id, text, name }) => (
+        <Fragment key={id}>
+          <label htmlFor={id}>{text}</label>
+          <input
+            type="color"
+            id={id}
+            name={name}
+            value={color[id]}
+            onChange={handleChange}
+          />
+        </Fragment>
+      ))}
+    </Fragment>
   );
 }
-
-const StyledLabel = styled.label`
-  display: flex;
-  margin: 10px;
-`;
--- pages/index.js
+++ pages/index.js
@@ -3,39 +3,23 @@ import { useState } from "react";
 import Link from "next/link";
 
 export default function HomePage() {
-  const [color1, setColor1] = useState("");
-  const [color2, setColor2] = useState("");
-  const [color3, setColor3] = useState("");
+  const [color, setColor] = useState({
+    color1: "",
+    color2: "",
+    color3: "",
+  });
+  const handleColorChange = (id, value) => {
+    setColor((prevColor) => ({
+      ...prevColor,
+      [id]: value,
+    }));
+  };
 
-  const handleColor1Change = (event) => {
-    const newColor = event.target.value;
-    setColor1(newColor);
-  };
-  const handleColor2Change = (event) => {
-    const newColor = event.target.value;
-    setColor2(newColor);
-  };
-  const handleColor3Change = (event) => {
-    const newColor = event.target.value;
-    setColor3(newColor);
-  };
+  console.log(color);
+
   return (
     <>
-      <MainColor
-        text="color1"
-        id="color1"
-        handleColorChange={handleColor1Change}
-      />
-      <MainColor
-        text="color2"
-        id="color2"
-        handleColorChange={handleColor2Change}
-      />
-      <MainColor
-        text="color3"
-        id="color3"
-        handleColorChange={handleColor3Change}
-      />{" "}
+      <MainColor color={color} handleColorChange={handleColorChange} />
       <Link href="/suggestions-page">
         <button type="button">Color Me!</button>
       </Link>
