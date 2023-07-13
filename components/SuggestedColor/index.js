a6a9f3a222d77708a9f13e792454d05c5538bb48
Nils <125801955+NilsOt1@users.noreply.github.com>
2023-07-13 12:24:22 +0200

    Update components/SuggestedColor/index.js
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>

--- components/SuggestedColor/index.js
+++ components/SuggestedColor/index.js
@@ -2,8 +2,6 @@ import { StyledDiv } from "./styles";
 
 export default function SuggestedColor({ text, color }) {
   return (
-    <>
       <StyledDiv color={color}>{text}</StyledDiv>
-    </>
   );
 }
