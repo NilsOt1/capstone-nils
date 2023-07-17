104656bf6811cbe8b4f4ca3ac0532ce05086f547
Nils Otto <nilsotto@gmx.de>
2023-07-17 17:05:13 +0200

    add form state and cancel/new-room buttons

--- pages/roomselection/index.js
+++ pages/roomselection/index.js
@@ -1,11 +1,29 @@
 import RoomSelectionForm from "@/components/RoomSelection/Form";
 import List from "@/components/RoomList";
+import { useState } from "react";
 
 export default function RoomSelectionPage({ handleCreateRoom, rooms }) {
+  const [showRoomForm, setShowRoomForm] = useState(false);
+
+  function handleNewRoomClick() {
+    setShowRoomForm(true);
+  }
+
+  function handleFormClose() {
+    setShowRoomForm(false);
+  }
+
   return (
     <>
       <List rooms={rooms} />
-      <RoomSelectionForm onCreateRoom={handleCreateRoom} />
+      {showRoomForm ? (
+        <>
+          <RoomSelectionForm onCreateRoom={handleCreateRoom} />
+          <button onClick={handleFormClose}>Cancle</button>
+        </>
+      ) : (
+        <button onClick={handleNewRoomClick}>New room</button>
+      )}
     </>
   );
 }
