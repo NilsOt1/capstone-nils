de8e1930522cd9714ee8ef9bf696d0bc6ad294f9
Nils Otto <nilsotto@gmx.de>
2023-07-20 22:10:07 +0200

    combine colorpage and suggestions

--- pages/choosecolorpage/index.js
+++ pages/choosecolorpage/index.js
@@ -1,10 +1,12 @@
 import MainColor from "@/components/MainColor";
 import Link from "next/link";
 import styled from "styled-components";
+import { StyledContainer } from "@/components/SuggestedColor/styles";
+import BackButton from "@/components/BackButton";
+import SuggestedColor from "@/components/SuggestedColor";
 
 const StyledLink = styled(Link)`
   text-align: center;
-  //padding: 15px 40px;
   border: none;
   border-radius: 100px;
   text-decoration: none;
@@ -23,9 +25,16 @@ const StyledMain = styled.div`
 
 export default function ChooseColor({ color, handleColorChange, rooms }) {
   return (
-    <StyledMain>
-      <MainColor color={color} handleColorChange={handleColorChange} />
-      <StyledLink href="/suggestionpage">Color Me!</StyledLink>
-    </StyledMain>
+    <>
+      <StyledMain>
+        <BackButton />
+        <MainColor color={color} handleColorChange={handleColorChange} />
+        <StyledLink href="/suggestionpage">Color Me!</StyledLink>
+      </StyledMain>
+      <StyledContainer>
+        <SuggestedColor color={color.color1}>Color Suggestion 1</SuggestedColor>
+        <SuggestedColor color={color.color2}>Color Suggestion 2</SuggestedColor>
+      </StyledContainer>
+    </>
   );
 }
